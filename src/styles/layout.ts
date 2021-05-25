09941286905b4c0fc8bdf8418b5e698144a40bae
favity <favius983@gmail.com>
2021-05-25 19:33:52 +0900

    wip

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,11 +1,16 @@
 import { css } from "goober";
 
+export const FullSize = css`
+  width: 100%;
+  height: 100%;
+`;
+
 export const Center = css`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const FrozenItem = css`
+export const StaticItem = css`
   flex: 0 0 auto;
 `;
